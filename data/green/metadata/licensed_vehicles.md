4089f2f2fdea2dc3b2eda6e74bb8046a7e81e5f6
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2019-06-08 11:41:54 +0100

    Recalculated count of licensed EVs and ULEVs

--- data/green/metadata/licensed_vehicles.md
+++ data/green/metadata/licensed_vehicles.md
@@ -1,4 +1,8 @@
-Number of licensed vehicles. Please note that diesel vehicles include cars and vans.
+Number of licensed vehicles. 
+
+- Diesel vehicles include cars and vans.
+- Ultra Low Emissions Vehicles (ULEVs) are electric or hybrid vehicles that emit less than 75g of carbon dioxide (CO<sub>2</sub>) per kilometre travelled. 
+- Electric vehicles (EVs) produce zero tail-pipe emissions and run on electric power only.
 
 Source: <a href="https://www.gov.uk/government/statistical-data-sets/all-vehicles-veh01" target="_blank">Department for Transport and Driver and Vehicle Licensing Agency</a>
 
--- data/green/pre-processing/licensed_vehicles.R
+++ data/green/pre-processing/licensed_vehicles.R
@@ -19,12 +19,13 @@ sheets <- tmp %>%
 
 diesel <- sheets %>% 
   filter(`Region/Local Authority` %in% c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", "Salford", "Stockport", "Tameside", "Trafford", "Wigan")) %>% 
-  mutate(value = as.numeric(`Diesel Cars`)+as.numeric(`Diesel Vans`),
+  mutate(period = as.Date(paste(sheet, 1, 1, sep = "-")),
+         value = as.numeric(`Diesel Cars`)+as.numeric(`Diesel Vans`),
          value = value*1000,
          group = "Diesel vehicles") %>% 
   select(area_code = `ONS LA Code`, 
          area_name = `Region/Local Authority`, 
-         period = sheet, 
+         period,
          value, 
          group)
 
@@ -33,32 +34,44 @@ tmp <- tempfile(fileext = ".ods")
 GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/794447/veh0131.ods",
     write_disk(tmp))
 
-electric <- read_ods(tmp, skip = 6)  %>%  
+ev <- read_ods(tmp, skip = 6)  %>%  
   filter(`Region/Local Authority` %in% c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", "Salford", "Stockport", "Tameside", "Trafford", "Wigan")) %>% 
   rename(area_code = `ONS LA Code`, area_name = `Region/Local Authority`) %>% 
   gather(period, value, -area_code, -area_name) %>% 
-  mutate(period = factor(parse_number(period)),
-         value = as.numeric(na_if(value, "c"))) %>% 
-  group_by(period, area_code, area_name) %>% 
-  summarise(value = sum(value)) %>% 
-  mutate(group = "Electric vehicles")
+  mutate(quarter = 
+           case_when(
+             str_detect(period, "Q1") ~ "01-01",
+             str_detect(period, "Q2") ~ "04-01",
+             str_detect(period, "Q3") ~ "07-01",
+             str_detect(period, "Q4") ~ "10-01"),
+         period = parse_number(period),
+         value = as.numeric(na_if(value, "c")),
+         group = "Electric vehicles") %>% 
+  unite(period, c("period", "quarter"), sep = "-") %>% 
+  mutate(period = as.Date(period, format = "%Y-%m-%d"))
 
 # Ultra low emission vehicles  
 tmp <- tempfile(fileext = ".ods")
 GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/794448/veh0132.ods",
     write_disk(tmp))
 
-ultra_low <- read_ods(tmp, skip = 6)  %>%  
+ulev <- read_ods(tmp, skip = 6)  %>%  
   filter(`Region/Local Authority` %in% c("Bolton", "Bury", "Manchester", "Oldham", "Rochdale", "Salford", "Stockport", "Tameside", "Trafford", "Wigan")) %>% 
   rename(area_code = `ONS LA Code`, area_name = `Region/Local Authority`) %>% 
   gather(period, value, -area_code, -area_name) %>% 
-  mutate(period = factor(parse_number(period)),
-         value = as.numeric(na_if(value, "c"))) %>% 
-  group_by(period, area_code, area_name) %>% 
-  summarise(value = sum(value)) %>% 
-  mutate(group = "Ultra low emission vehicles")
+  mutate(quarter = 
+           case_when(
+             str_detect(period, "Q1") ~ "01-01",
+             str_detect(period, "Q2") ~ "04-01",
+             str_detect(period, "Q3") ~ "07-01",
+             str_detect(period, "Q4") ~ "10-01"),
+         period = parse_number(period),
+         value = as.numeric(na_if(value, "c")),
+         group = "Ultra low emission vehicles") %>% 
+  unite(period, c("period", "quarter"), sep = "-") %>% 
+  mutate(period = as.Date(period, format = "%Y-%m-%d"))
 
-df <- bind_rows(diesel, electric, ultra_low) %>% 
+df <- bind_rows(diesel, ev, ulev) %>% 
   mutate(indicator = "Vehicles",
          measure = "Frequency",
          unit = "Vehicles") %>% 
