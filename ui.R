8105e9a142b95500e1b830b9224b13bd053c5169
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-06-26 12:28:04 +0100

    Fixed text cutoff

--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 ui <- fluidPage(
+  HTML('<meta name="viewport" content="width=1024">'),
   theme = shinytheme("yeti"),
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
@@ -27,75 +28,68 @@ ui <- fluidPage(
         )
       ),
       br(),
-      fluidRow(column(
-        11,
+      fluidRow(
+        column(11,
         div(
-          div(),
           tags$button(
             id = "housing_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/housing.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/housing.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Affordable and quality homes", '<br />', em("Trafford has a choice of quality homes that people can afford."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "health_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/health.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/health.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Health and wellbeing", '<br />', em("Trafford has improved health and wellbeing, and reduced health inequalities."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "places_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/places.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/places.png", height = "50px", style = "float: left; margin-right:1 em;"),
             HTML(paste('<p align="left">', "Successful and thriving places", '<br />', em("Trafford has successful and thriving town centres and communities."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "children_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/children.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/children.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Children and young people", '<br />', em("All children and young people in Trafford will have a fair start."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "pride_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/pride.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/pride.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Pride in our area", '<br />', em("People in Trafford will take pride in their local area."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "green_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/green.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/green.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Green and connected", '<br />', em("Trafford will maximise its green spaces, transport and digital connectivity."), '</p>'))
           )
         ),
         div(
-          div(),
           tags$button(
             id = "targeted_tab",
             class = "btn action-button",
-            style = "color: #212121; background-color: #fff; border: none;",
-            tags$img(src = "icons/targeted.png", height = "50px", style = "float: left; margin-right:1rem;"),
+            style = "color: #212121; background-color: #fff; border: none; width: 100%;",
+            tags$img(src = "icons/targeted.png", height = "50px", style = "float: left; margin-right: 1em;"),
             HTML(paste('<p align="left">', "Targeted support", '<br />', em("People in Trafford will get support when they need it most."), '</p>'))
           )
         )
